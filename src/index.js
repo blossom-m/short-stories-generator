abe0e2fad376a6eb9b2639f099f3944c8ff4925c
blossom-m <113142866+blossom-m@users.noreply.github.com>
2024-10-24 20:39:42 +0200

    Added the basic JavaScript form handling and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateStory(event) {
+  event.preventDefault();
 
+  new Typewriter("#story", {
+    strings: "It was a glorious, colorful autumn.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let storyFormElement = document.querySelector("#story-generator-form");
+storyFormElement.addEventListener("submit", generateStory);
