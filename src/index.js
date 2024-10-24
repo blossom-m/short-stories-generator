47b4cf4052eac1138a6368e799fad2aeb9586bbf
blossom-m <113142866+blossom-m@users.noreply.github.com>
2024-10-24 22:30:24 +0200

    Added the AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-function generateStory(event) {
-  event.preventDefault();
+function displayStory(response) {
+  console.log("story generated");
 
   new Typewriter("#story", {
-    strings: "It was a glorious, colorful autumn.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generateStory(event) {
+  event.preventDefault();
+
+  let textInputElement = document.querySelector("#text-input");
+  let apiKey = "3ac30o3a6617501a8ba14f0ect4c9d11";
+  let context =
+    "You are a romantic Story expert and love to write short stories. Your mission is to generate a one paragraph story. Please make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate a short story about ${textInputElement.value}`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating story");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayStory);
 }
 
 let storyFormElement = document.querySelector("#story-generator-form");
